58e67bf257cefa223c9fe0105a1fef043d0df12b
MarkustheSlime <112181443+MarkustheSlime@users.noreply.github.com>
2022-08-26 09:53:07 -0400

    added important comment

--- src/main/java/net/MarkustheSlime/tutorialmod/Tutorialmod.java
+++ src/main/java/net/MarkustheSlime/tutorialmod/Tutorialmod.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 @Mod(Tutorialmod.MOD_ID)
 public class Tutorialmod
 {
+    //comment
     public static final String MOD_ID = "tutorialmod";
     private static final Logger LOGGER = LogUtils.getLogger();
 
