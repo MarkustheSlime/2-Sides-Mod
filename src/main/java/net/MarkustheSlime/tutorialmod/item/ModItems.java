6e5dc36fc4e47c7dcec9ac0480e7f4fd504e17ac
MarkustheSlime <112181443+MarkustheSlime@users.noreply.github.com>
2022-08-29 20:19:54 -0400

    added geode ores and shards; added meteorite and debris

--- src/main/java/net/MarkustheSlime/tutorialmod/item/ModItems.java
+++ src/main/java/net/MarkustheSlime/tutorialmod/item/ModItems.java
@@ -18,6 +18,23 @@ public class ModItems {
     public static final RegistryObject<Item> DM_Shard = ITEMS.register("dm_shard",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
 
+    public static final RegistryObject<Item> SOUL_Shard = ITEMS.register("soul_shard",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> LIFE_Shard = ITEMS.register("life_shard",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> POWER_Shard = ITEMS.register("power_shard",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> CONTROL_Shard = ITEMS.register("control_shard",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> PRIMAL_Shard = ITEMS.register("primal_shard",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> INFLUENCE_Shard = ITEMS.register("influence_shard",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> VITALITY_Shard = ITEMS.register("vitality_shard",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> METEORITE_DEBRIS = ITEMS.register("meteorite_debris",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+
     public static final RegistryObject<Item> TESTADVANCEDITEM = ITEMS.register("testadvanceditem",
             () -> new TESTADVANCEDITEM(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).stacksTo(1)));
     public static void register(IEventBus eventBus) {
